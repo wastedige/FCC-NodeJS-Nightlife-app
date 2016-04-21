c33eeae7a6445c559a530667d4a5b701d584b2da
Shaahin <shaahin@gmail.com>
2016-04-20 23:28:52 -0500

    testing num param

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/:num', function(request, response) {
+  console.log(num);
   response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-

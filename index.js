665e010092b4de29d79b68d30893a86ba72f28fa
Shaahin <shaahin@gmail.com>
2016-05-19 20:27:15 -0500

    Auth fully working

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ app.use(cookieParser());
 app.use(session({
   secret: 'some crappy secret'
 }));
-// app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
+// http://codetheory.in/how-to-use-twitter-oauth-with-node-oauth-in-your-node-js-express-application/
 var OAuth = require('oauth').OAuth
 var oauth = new OAuth(
       "https://api.twitter.com/oauth/request_token",
@@ -21,7 +17,7 @@ var oauth = new OAuth(
       "xh3hmd97trRCzGjC7sBpUvsvD",
       "7LD7EGf7QNQSvyJ313unTPf8q6agsSYFrHK5lq2OfGoonrxV1R",
       "1.0",
-      "http://yoursite.com/auth/twitter/callback",
+      "http://localhost:5000/auth/twitter/callback",
       "HMAC-SHA1"
     );
 
@@ -42,10 +38,42 @@ app.get('/auth/twitter', function(req, res) {
         token: oauth_token,
         token_secret: oauth_token_secret
       };
-      console.log(req.session.oauth);
+      console.log("init auth", req.session.oauth);
       res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
     }
   });
+
+});
+
+app.get('/auth/twitter/callback', function(req, res, next) {
+
+  if (req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier;
+    var oauth_data = req.session.oauth;
+
+    oauth.getOAuthAccessToken(
+      oauth_data.token,
+      oauth_data.token_secret,
+      oauth_data.verifier,
+      function(error, oauth_access_token, oauth_access_token_secret, results) {
+        if (error) {
+          console.log(error);
+          res.send("Authentication Failure!");
+        }
+        else {
+          req.session.oauth.access_token = oauth_access_token;
+          req.session.oauth.access_token_secret = oauth_access_token_secret;
+          console.log("results", req.session.oauth);
+          res.send("Authentication Successful");
+          // res.redirect('/'); // You might actually want to redirect!
+        }
+      }
+    );
+  }
+  else {
+    res.redirect('/login'); // Redirect to login page
+    console.log("no token?", req.session.oauth);
+  }
 
 });
 
